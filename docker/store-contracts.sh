f3aa37d969d3b5e942d3654a7642939b6eb94d8f
bwty <whalelephant@users.noreply.github.com>
2021-12-02 17:37:47 +0000

    fix deploy nft with more gas

--- docker/store-contracts.sh
+++ docker/store-contracts.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -ex
 
-secretd tx compute store /root/code/pj-dao/contract.wasm.gz --from a --gas 3000000 -y --keyring-backend test
-sleep 5 
-secretd tx compute store /root/code/pj-nft/contract.wasm.gz --from a --gas 3000000 -y --keyring-backend test
+secretd tx compute store /root/code/pj-dao/contract.wasm.gz --from a --gas 30000000 -y --keyring-backend test
+secretd tx compute store /root/code/pj-nft/contract.wasm.gz --from b --gas 30000000 -y --keyring-backend test
 sleep 5 
 RES=$(secretd query compute list-code)
-DAO_CODE_ID=$(echo $RES | jq -r '.[-2].id')
+DAO_CODE_ID=$(echo $RES | jq -r '.[-1].id')
 NFT_CODE_ID=$(echo $RES | jq -r '.[-1].id')
 
 INIT='{}'
 secretd tx compute instantiate $DAO_CODE_ID "$INIT" --from a --label "pj-dao-$DAO_CODE_ID" -y --keyring-backend test
-sleep 5 
 secretd tx compute instantiate $NFT_CODE_ID "$INIT" --from b --label "pj-nft-$NFT_CODE_ID" -y --keyring-backend test
 sleep 5 
+
 DAO_ADDRESS=$(secretd query compute list-contract-by-code $DAO_CODE_ID)
 NFT_ADDRESS=$(secretd query compute list-contract-by-code $NFT_CODE_ID) 
 
